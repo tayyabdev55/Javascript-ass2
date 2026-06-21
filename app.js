d0a0f93fae18d9f5c34fe4714a2fde946c096877
tayyabdev55 <tayyabkhan89807@gmail.com>
2026-06-21 06:14:29 +0500

    Java script button function

---
+++ app.js
@@ -0,0 +1,19 @@
+function ChangeColorGreen() {
+    document.body.style.backgroundColor = "#658354"
+}
+
+function ChangeColorRed() {
+    document.body.style.backgroundColor = "#A4133C"
+}
+
+function ChangeColorBlue() {
+    document.body.style.backgroundColor = "#354c7c"
+}
+
+function ChangeColorOrange() {
+     document.body.style.backgroundColor = "#ec6d"
+}
+
+function ResetColor() {
+    document.body.style.backgroundColor = "";
+}
